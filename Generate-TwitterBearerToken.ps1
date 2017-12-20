16df28fc479ca6777663cc432cb82ecf3d4f80a2
Raphael <raphael@haddad.net.au>
2017-12-21 08:44:12 +1100

    Add script to generate twitter bearer token

---
+++ Generate-TwitterBearerToken.ps1
@@ -0,0 +1,38 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]
+    $ConsumerKey,
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $ConsumerSecret
+)
+
+function Generate-Base64Encoding($consumerKey, $consumerSecret)
+{
+    $combined = "$consumerKey$consumerSecret".Replace(' ', ':')
+    $bytes = [System.Text.Encoding]::UTF8.GetBytes($combined)
+    $encodedSecrets = [System.Convert]::ToBase64String($bytes)
+    return $encodedSecrets
+}
+
+function Obtain-BearerToken($secrets)
+{
+    $authHeader = "Basic $secrets"
+    $headers = @{
+        "Authorization" = $authHeader;
+        "Content-Type" = "application/x-www-form-urlencoded;charset=UTF-8.";
+    }
+
+    $body = "grant_type=client_credentials"
+
+    $token = Invoke-RestMethod -Uri "https://api.twitter.com/oauth2/token" `
+                               -Method POST `
+                               -Headers $headers `
+                               -Body $body
+    return $token.access_token
+}
+
+$encoded = Generate-Base64Encoding($ConsumerKey, $ConsumerSecret)
+$bearerToken = Obtain-BearerToken($encoded)
+Write-Output $bearerToken
---
+++ README.md
@@ -0,0 +1,9 @@
+This is a PowerShell script that generates a Twitter [bearer token](https://developer.twitter.com/en/docs/basics/authentication/overview/application-only) to be used for Twitter API calls.
+
+Usuage:
+
+```PowerShell
+.\Generate-TwitterBearerToken.ps1 -ConsumerKey "xvz1evFS4wEEPTGEFPHBog" -ConsumerSecret "L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg"
+```
+
+You can get your specific application's ConsumerKey and ConsumerSecret from the [Twitter Application page](https://apps.twitter.com/)
